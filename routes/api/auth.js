94a160520b58f202d440296b64a8f7410ab5ab30
Roddy <roddycarson@gmail.com>
2021-01-26 19:31:24 +0000

    routes

---
+++ routes/api/auth.js
@@ -0,0 +1,13 @@
+//handle jsonwebtoken for authentication
+//handle adding users
+
+const express = require('express');
+const router = express.Router();
+
+//@route Get api/auth
+//@desc Test route
+//@access Public
+
+router.get('/', (req, res) => res.send('Auth route'));
+
+module.exports = router;
