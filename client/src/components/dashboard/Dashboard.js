148a8141d3d4e68d00b156353af6d0b6ad8533af
Roddy <roddycarson@gmail.com>
2021-04-08 18:32:38 +0100

    Dashboard layout

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = (props) => {
-  return <div>Dashboard</div>;
+  return (
+    <section class='dash'>
+      <div class=' profile-img'></div>
+      <h1 class='large text-primary'>Dashboard</h1>
+      <p class='lead'>
+        <i class='fas fa-user'></i> Use these MBI techniques for better study
+        management skills.
+      </p>
+      <div>
+        <Link to='/planner' class='btn btn-light'>
+          <i class='fas fa-user-circle text-primary'></i> Planner
+        </Link>
+        <Link to='/journal' class='btn btn-light'>
+          <i class='fab fa-black-tie text-primary'></i> Journal
+        </Link>
+        <Link to='/workshop' class='btn btn-light'>
+          <i class='fas fa-graduation-cap text-primary'></i> Workshop
+        </Link>
+      </div>
+      <div class='recent'>
+        <h1 class='large text-primary'>Recent Activity</h1>
+      </div>
+    </section>
+  );
 };
 
 Dashboard.propTypes = {};
